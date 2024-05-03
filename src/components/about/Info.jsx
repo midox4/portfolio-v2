0ec814ed54c0da5aa0a6b2c6fa6b28beeb9cbfce
Hafsi Ahmed <crasypo@gmail.com>
2024-05-03 17:18:51 +0100

    Update Info.jsx

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -17,7 +17,7 @@ const Info = () => {
         <div className="about__box">
             <FiPhoneCall className="about__icon" />
             <h3 className="about__title">Phone</h3>
-            <span className="about__subtitle">+1 (506) 899-7431</span>
+            <span className="about__subtitle">+1(506)899-7431</span>
         </div>
     </div>
   );
